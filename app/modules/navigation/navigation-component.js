467f41ea5db10a3965f165caf33dc12af5788ce3
iisha small <iishasmall@iishas-MacBook-Pro.local>
2016-12-20 12:38:52 -0500

    adding new functionality to about section with rollovers and fades

--- app/modules/navigation/navigation-component.js
+++ app/modules/navigation/navigation-component.js
@@ -10,19 +10,7 @@ navSection.component('navSection',{
 
 		var self = this;
 
-		/*$document.on('scroll', function() {
-      console.log('Document scrolled to ', $document.scrollLeft(), $document.scrollTop());
-    });*/
-
-   /* var someElement = angular.element(document.getElementById('home-section'));
-    var offset = 60;
-    var duration =2000;
-    $document.scrollToElement(someElement, offset, duration);*/
-
-    /*var container = angular.element(document.getElementById('home-section'));
-    container.on('scroll', function() {
-      console.log('Container scrolled to ', container.scrollLeft(), container.scrollTop());
-    });*/
+		
 		$scope.clickThis = function (href){
 			/*console.log("clicked");*/
 			
@@ -36,7 +24,7 @@ navSection.component('navSection',{
 
 
 
-}).value('duScrollOffset',300);
+}).value('duScrollOffset',50);
 
 
 
